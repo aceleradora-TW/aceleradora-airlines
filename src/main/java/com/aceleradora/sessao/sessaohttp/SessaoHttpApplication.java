10fe37992c3aca0a06f56dafe7ca3235566441f3
yrachid <anuar.yasser@gmail.com>
2020-08-05 22:42:05 -0300

    Spring initializr

---
+++ src/main/java/com/aceleradora/sessao/sessaohttp/SessaoHttpApplication.java
@@ -0,0 +1,13 @@
+package com.aceleradora.sessao.sessaohttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaoHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleradora/sessao/sessaohttp/SessaoHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleradora.sessao.sessaohttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessaoHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
