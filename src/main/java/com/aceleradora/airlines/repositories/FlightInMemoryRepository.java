97c80aadaee0cdb4fe8baeaf77505ecf9abd5ed5
yrachid <anuar.yasser@gmail.com>
2020-08-06 12:36:36 -0300

    Adds more flights

--- src/main/java/com/aceleradora/airlines/repositories/FlightInMemoryRepository.java
+++ src/main/java/com/aceleradora/airlines/repositories/FlightInMemoryRepository.java
@@ -1,10 +1,10 @@
 package com.aceleradora.airlines.repositories;
 
 import com.aceleradora.airlines.domain.CountryCode;
-import com.aceleradora.airlines.domain.flight.Flight;
-import com.aceleradora.airlines.domain.flight.FlightNumber;
 import com.aceleradora.airlines.domain.IataCode;
 import com.aceleradora.airlines.domain.Position;
+import com.aceleradora.airlines.domain.flight.Flight;
+import com.aceleradora.airlines.domain.flight.FlightNumber;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -12,46 +12,79 @@ import java.util.List;
 
 @Repository
 public class FlightInMemoryRepository {
-    private static final CountryCode BRASIL = CountryCode.parse("BR");
-    private static final CountryCode ARGENTINA = CountryCode.parse("AR");
-    private static final CountryCode CHILE = CountryCode.parse("CL");
-    private static final IataCode PORTO_ALEGRE = IataCode.parse("POA");
-    private static final IataCode RIO_DE_JANEIRO = IataCode.parse("RIO");
-    private static final IataCode SAO_PAULO = IataCode.parse("SAO");
-    private static final IataCode CONGONHAS = IataCode.parse("CGH");
-    private static final IataCode SALGADO_FILHO = IataCode.parse("POA");
-    private static final IataCode BUENOS_AIRES = IataCode.parse("BUE");
-    private static final IataCode AEROPARQUE_JORGE_NEWBERY = IataCode.parse("AEP");
-    private static final IataCode SANTIAGO = IataCode.parse("SCL");
-    private static final IataCode COMODORO_ARTURO_MERINO_BENITEZ = IataCode.parse("SCL");
-    private static final IataCode GALEAO = IataCode.parse("GIG");
-    private static final IataCode GUARULHOS = IataCode.parse("GRU");
+
+    // Cities and airports
+    private static final String AEP = "AEP";
+    private static final String BUE = "BUE";
+    private static final String CGH = "CGH";
+    private static final String CWB = "CWB";
+    private static final String FLN = "CWB";
+    private static final String GIG = "GIG";
+    private static final String GRU = "GRU";
+    private static final String JFK = "JFK";
+    private static final String MVD = "MVD";
+    private static final String NYC = "NYC";
+    private static final String POA = "POA";
+    private static final String RIO = "RIO";
+    private static final String SAO = "SAO";
+    private static final String SCL = "SCL";
+
+    // Countries
+    private static final String AR = "AR";
+    private static final String BR = "BR";
+    private static final String CL = "BR";
+    private static final String US = "US";
+    private static final String UY = "UY";
+
+    // Carriers
+    private static final String AA = "AA";
+    private static final String AZU = "AZU";
+    private static final String G3 = "G3";
+    private static final String LAN = "LAN";
+
+    private static final List<Flight> AVAILABLE_FLIGHTS = List.of(
+            flight(number(AA, "1012"), departing(JFK, NYC, US), arriving(GRU, SAO, BR)),
+            flight(number(AA, "1013"), departing(GRU, SAO, BR), arriving(SCL, CL)),
+            flight(number(AA, "1014"), departing(GRU, SAO, BR), arriving(CWB, CWB, BR)),
+            flight(number(AZU, "1060"), departing(POA, BR), arriving(CGH, SAO, BR)),
+            flight(number(AZU, "2030"), departing(POA, BR), arriving(AEP, BUE, AR)),
+            flight(number(AZU, "2035"), departing(POA, BR), arriving(FLN, BR)),
+            flight(number(AZU, "2036"), departing(FLN, BR), arriving(POA, BR)),
+            flight(number(LAN, "744"), departing(POA, BR), arriving(SCL, CL)),
+            flight(number(LAN, "1742"), departing(FLN, BR), arriving(CWB, BR)),
+            flight(number(LAN, "1747"), departing(CWB, BR), arriving(FLN, BR)),
+            flight(number(LAN, "1550"), departing(GIG, RIO, BR), arriving(POA, BR)),
+            flight(number(G3, "4550"), departing(SCL, CL), arriving(MVD, UY)),
+            flight(number(G3, "4543"), departing(MVD, UY), arriving(SCL, CL)),
+            flight(number(G3, "0442"), departing(MVD, UY), arriving(CWB, BR)),
+            flight(number(G3, "0440"), departing(CWB, BR), arriving(MVD, UY)),
+            flight(number(G3, "1011"), departing(SCL, CL), arriving(POA, BR)),
+            flight(number(G3, "1327"), departing(GRU, SAO, BR), arriving(GIG, RIO, BR)));
 
     public Collection<Flight> findAll() {
-        return List.of(
-                new Flight(
-                        new FlightNumber("AZU", "1060"),
-                        new Position(SALGADO_FILHO, PORTO_ALEGRE, BRASIL),
-                        new Position(CONGONHAS, SAO_PAULO, BRASIL)),
-                new Flight(
-                        new FlightNumber("AZU", "2030"),
-                        new Position(SALGADO_FILHO, PORTO_ALEGRE, BRASIL),
-                        new Position(AEROPARQUE_JORGE_NEWBERY, BUENOS_AIRES, ARGENTINA)),
-                new Flight(
-                        new FlightNumber("LAN", "744"),
-                        new Position(SALGADO_FILHO, PORTO_ALEGRE, BRASIL),
-                        new Position(COMODORO_ARTURO_MERINO_BENITEZ, SANTIAGO, CHILE)),
-                new Flight(
-                        new FlightNumber("LAN", "1550"),
-                        new Position(GALEAO, RIO_DE_JANEIRO, BRASIL),
-                        new Position(SALGADO_FILHO, PORTO_ALEGRE, BRASIL)),
-                new Flight(
-                        new FlightNumber("G3", "1011"),
-                        new Position(COMODORO_ARTURO_MERINO_BENITEZ, SANTIAGO, CHILE),
-                        new Position(SALGADO_FILHO, PORTO_ALEGRE, BRASIL)),
-                new Flight(
-                        new FlightNumber("G3", "1327"),
-                        new Position(GUARULHOS, SAO_PAULO, BRASIL),
-                        new Position(GALEAO, RIO_DE_JANEIRO, BRASIL)));
+        return AVAILABLE_FLIGHTS;
+    }
+
+    private static Flight flight(FlightNumber number, Position departure, Position arrival) {
+        return new Flight(number, departure, arrival);
+    }
+
+    private static FlightNumber number(String carrier, String number) {
+        return new FlightNumber(carrier, number);
+    }
+
+    private static Position departing(String airport, String country) {
+        return departing(airport, airport, country);
+    }
+
+    private static Position departing(String airport, String city, String country) {
+        return new Position(IataCode.parse(airport), IataCode.parse(city), CountryCode.parse(country));
+    }
+
+    private static Position arriving(String airport, String country) {
+        return arriving(airport, airport, country);
+    }
+    private static Position arriving(String airport, String city, String country) {
+        return new Position(IataCode.parse(airport), IataCode.parse(city), CountryCode.parse(country));
     }
 }
